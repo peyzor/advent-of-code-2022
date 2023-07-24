5d418b86bb9e3c2f880b7b2c880b18357d0caae4
Peyman <hassani.peyman627@gmail.com>
2023-07-24 14:24:50 +0330

    Add day7 part2 solution

--- Day7/main.go
+++ Day7/main.go
@@ -2,4 +2,5 @@ package main
 
 func main() {
 	day7part1()
+	day7part2()
 }
---
+++ Day7/part2.go
@@ -0,0 +1,117 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"path/filepath"
+	"strconv"
+	"strings"
+)
+
+func day7part2() {
+	relativePath := "Day7/input.txt"
+
+	currentDir, err := os.Getwd()
+	if err != nil {
+		log.Fatal(err)
+	}
+	inputPath := filepath.Join(currentDir, relativePath)
+
+	file, err := os.Open(inputPath)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer func(file *os.File) {
+		err := file.Close()
+		if err != nil {
+			log.Fatal(err)
+		}
+	}(file)
+
+	var rootDir *Node
+	var currDir *Node
+	var dirs []*Node
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		line := strings.TrimSpace(scanner.Text())
+		input := strings.Fields(line)
+
+		if len(input) > 2 {
+			var destination string
+			_, err := fmt.Sscanf(line, "$ cd %s", &destination)
+			if err != nil {
+				log.Fatal(err)
+			}
+			if destination == ".." {
+				currDir = currDir.Parent
+
+			} else if destination == "/" {
+				currDir = &Node{
+					Name:     "/",
+					Size:     0,
+					IsFile:   false,
+					Parent:   nil,
+					Children: make(map[string]*Node),
+				}
+				rootDir = currDir
+
+			} else {
+				currDir = currDir.Children[destination]
+			}
+
+		} else if input[0] == "dir" {
+			dirName := input[1]
+
+			currDir.Children[dirName] = &Node{
+				Name:     dirName,
+				Size:     0,
+				IsFile:   false,
+				Parent:   currDir,
+				Children: make(map[string]*Node),
+			}
+			dirs = append(dirs, currDir.Children[dirName])
+
+		} else if input[0] != "$" {
+			size, _ := strconv.Atoi(input[0])
+			filename := input[1]
+
+			currDir.Children[filename] = &Node{
+				Name:     filename,
+				Size:     size,
+				IsFile:   true,
+				Parent:   currDir,
+				Children: nil,
+			}
+		}
+	}
+
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+
+	rootSize := calcSize(*rootDir)
+	totalAvailable := 70_000_000
+	needed := 30_000_000
+	availableNow := totalAvailable - rootSize
+	needToDelete := needed - availableNow
+
+	var candidates []*Node
+	for _, dir := range dirs {
+		size := calcSize(*dir)
+		dir.Size = size
+		if size >= needToDelete {
+			candidates = append(candidates, dir)
+		}
+	}
+
+	smallestCandidate := candidates[0]
+	for _, c := range candidates {
+		if c.Size < smallestCandidate.Size {
+			smallestCandidate = c
+		}
+	}
+
+	fmt.Printf("%d\n", smallestCandidate.Size)
+}
